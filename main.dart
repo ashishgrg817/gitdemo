3ac16eab0ed5791326f8378ad033e01100d50946
Rohit <ashishgrg.817@gmail.com>
2021-05-14 13:49:25 +0530

    First Commit

---
+++ main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flushbar/flushbar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter FlushBar Example ',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('flush'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text(
+            "ClickMe",
+            style: TextStyle(color: Colors.white),
+          ),
+          color: Colors.orange,
+          onPressed: () {
+            show_Simple_Flushbar(context);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+//Simple Flushbar with a button
+void show_Simple_Flushbar(BuildContext context) {
+  Flushbar(
+    message: 'Hello from a Flushbar',
+    duration: Duration(seconds: 3),
+  )..show(context);
+}
