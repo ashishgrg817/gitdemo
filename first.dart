4344884cdfcdf525c9da4caccfe22bf53832c027
Rohit <ashishgrg.817@gmail.com>
2021-05-14 14:01:49 +0530

    Add new file and commit

---
+++ first.dart
@@ -0,0 +1,22 @@
+class Ali extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('flush'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text(
+            "ClickMe",
+            style: TextStyle(color: Colors.white),
+          ),
+          color: Colors.orange,
+          onPressed: () {
+            show_Simple_Flushbar(context);
+          },
+        ),
+      ),
+    );
+  }
+}
